221827deb9f58257151479c806ba3f23bfe9733c
sj-lijl <lijinlong@zhonghuitech.cn>
2023-03-13 12:48:00 +0800

    timer: 添加执行一次定时器

--- timer_test.go
+++ timer_test.go
@@ -6,13 +6,13 @@ import (
 	"time"
 )
 
-
+//
 func Test_SetInterval(t *testing.T) {
 	SetInterval(time.Second*2, func() {
 		println("SetInterval------")
 	})
 }
-
+//
 func Test_SetTimer(t *testing.T) {
 	SetTimer(time.Second, func() {
 		println("Test_SetTimer------")
